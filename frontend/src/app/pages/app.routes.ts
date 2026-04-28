fedb0c463c64a61e629914178a2e3e8644456247
Junior Castilla <juniorcastillamarcos@gmail.com>
2026-04-27 23:08:45 -0500

    Mejoras en el ffooter y en el navbar, agregando la pagina de catalogo

--- frontend/src/app/pages/app.routes.ts
+++ frontend/src/app/pages/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
-import { InicioComponent } from './inicio'; // Importamos lo que acabas de crear
+import { InicioComponent } from './inicio'; // Tu importación original
+
+// Importación corregida: apuntamos directamente a la carpeta catalogo
+import { CatalogoComponent } from './catalogo/catalogo'; 
 
 export const routes: Routes = [
-  { path: '', component: InicioComponent }, // Ruta vacía = Tu Index
+  { path: '', component: InicioComponent }, // Tu ruta de inicio original que no tocamos
+  { path: 'catalogo', component: CatalogoComponent }, // La nueva ruta
 ];
