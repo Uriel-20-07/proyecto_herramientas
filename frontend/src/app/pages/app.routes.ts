2dec5e5f422d1b72c6677de1ac8e58963cb577de
Aldo <aldofer@gmail.com>
2026-04-28 00:05:24 -0500

    Implementación  blog

--- frontend/src/app/pages/app.routes.ts
+++ frontend/src/app/pages/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import { InicioComponent } from './inicio'; // Importamos lo que acabas de crear
+import { InicioComponent } from './inicio';
+import { BlogComponent } from './blog/blog.component';
 
 export const routes: Routes = [
-  { path: '', component: InicioComponent }, // Ruta vacía = Tu Index
+  { path: '', component: InicioComponent },
+  { path: 'blog', component: BlogComponent },
 ];
