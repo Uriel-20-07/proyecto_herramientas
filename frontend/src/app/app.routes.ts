bafc83c78f0851d508f2cfd66c952f8d833e5ac1
Diana <U23221294@utp.edu.pe>
2026-05-17 01:10:55 -0500

    feat: integración de imágenes en el catálogo y actualización de rutas

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,36 @@
+import { Routes } from '@angular/router';
+import { BlogComponent } from './pages/blog/blog.component';
+import { InicioComponent } from './pages/inicio';
+import { CatalogoComponent } from './pages/catalogo/catalogo';
+import { CarritoComponent } from './pages/carrito/carrito';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { RecuperarContraseñaComponent } from './components/recuperar-contrasena/recuperar-contrasena.component';
+import { OfertasComponent } from './pages/ofertas/ofertas';
+import { MarcasComponent } from './pages/marcas/marcas';
+import { NosotrosComponent } from './pages/nosotros/nosotros';
+
+export const routes: Routes = [
+
+  { path: '', component: InicioComponent },
+
+  { path: 'blog', component: BlogComponent },
+  { path: 'catalogo', component: CatalogoComponent },
+  { path: 'carrito', component: CarritoComponent },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+
+  { path: 'recuperar-contrasena', component: RecuperarContraseñaComponent },
+  { path: 'reset-password/:token', component: RecuperarContraseñaComponent },
+
+  { path: 'ofertas', component: OfertasComponent },
+  { path: 'marcas', component: MarcasComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'perfil', component: LoginComponent },
+  { path: 'favoritos', component: LoginComponent },
+  { path: 'pedidos', component: LoginComponent },
+
+  { path: '**', component: InicioComponent }
+
+];
